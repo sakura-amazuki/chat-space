a67a323af75208ea98d298dcfe18eeea82a144ce
sakura-amazuki <sakura@sakuranoMacBook-puro.local>
2019-02-17 00:41:47 +0900

    Make

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resource :user, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
